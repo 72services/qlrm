928d84ca896b3323cb075948440e06af559e52ef
begert <begi@gmx.ch>
2013-10-22 12:20:44 +0200

    better error-handling on illegal parameter types

--- src/main/java/org/qlrm/mapper/JdbcResultMapper.java
+++ src/main/java/org/qlrm/mapper/JdbcResultMapper.java
@@ -38,6 +38,12 @@ public class JdbcResultMapper {
     private <T> T createBean(Constructor<?> ctor, Object[] args) {
         try {
             return (T) ctor.newInstance(args);
+        } catch (IllegalArgumentException e) {
+            StringBuilder sb = new StringBuilder("no constructor taking:\n");
+            for (Object object : args) {
+                sb.append("\t").append(object.getClass().getName()).append("\n");
+            }
+            throw new RuntimeException(sb.toString(), e);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
