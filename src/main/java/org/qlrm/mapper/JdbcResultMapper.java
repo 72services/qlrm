d28fd79fdc68bf9f29a7bcba2319750b62b14f81
begert <begi@gmx.ch>
2013-10-30 17:07:03 +0100

    get constructor by resultSet column types

--- src/main/java/org/qlrm/mapper/JdbcResultMapper.java
+++ src/main/java/org/qlrm/mapper/JdbcResultMapper.java
@@ -11,7 +11,7 @@ public class JdbcResultMapper extends ResultMapper {
     @SuppressWarnings("unchecked")
     public <T> List<T> list(ResultSet rs, Class<T> clazz) throws SQLException {
         List<T> result = new ArrayList<T>();
-        Constructor<?> ctor = (Constructor<?>) clazz.getDeclaredConstructors()[0];
+        Constructor<?> ctor = getConstructor(rs, clazz);
 
         while (rs.next()) {
             Object[] objs = new Object[ctor.getParameterTypes().length];
@@ -22,15 +22,39 @@ public class JdbcResultMapper extends ResultMapper {
         }
         return result;
     }
-
+	
     @SuppressWarnings("unchecked")
     public <T> T uniqueResult(ResultSet rs, Class<T> clazz) throws SQLException {
-        Constructor<T> ctor = (Constructor<T>) clazz.getDeclaredConstructors()[0];
+        Constructor<T> ctor = (Constructor<T>) getConstructor(rs, clazz);
         rs.next();
         Object[] objs = new Object[ctor.getParameterTypes().length];
         for (int i = 0; i < ctor.getParameterTypes().length; i++) {
             objs[i] = rs.getObject(i + 1);
         }
         return createInstance(ctor, objs);
+    }
+	
+	private Constructor<?> getConstructor(ResultSet rs, Class<?> clazz) throws SQLException {
+        Constructor<?> ctor = null;
+        outer: for (Constructor<?> ct : clazz.getDeclaredConstructors()) {
+            if (ct.getParameterTypes().length == rs.getMetaData().getColumnCount()) {
+                for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+                    if (!ct.getParameterTypes()[i - 1].getName().equals(
+                            rs.getMetaData().getColumnClassName(i))) {
+                        continue outer;
+                    }
+                }
+                ctor = ct;
+                break;
+            }
+        }
+        if (ctor == null) {
+            StringBuilder sb = new StringBuilder("no constructor taking: ");
+            for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+                sb.append(rs.getMetaData().getColumnClassName(i)).append(", ");
+            }
+            throw new RuntimeException(sb.toString());
+        }
+        return ctor;
     }
 }
