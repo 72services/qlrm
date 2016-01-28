5e36cd2f993e1790ebde3a9ab04795474a3dcc0b
Jan Mosig <jan.mosig@itemis.de>
2016-01-28 12:15:08 +0100

    Issue #9: Implemented post processing for raw JPA result data and updated unit tests accordingly.

--- src/test/java/org/qlrm/mapper/JpaResultMapperTest.java
+++ src/test/java/org/qlrm/mapper/JpaResultMapperTest.java
@@ -1,8 +1,5 @@
 package org.qlrm.mapper;
 
-import org.qlrm.generator.ClassGenerator;
-import org.qlrm.model.Employee;
-import org.qlrm.to.EmployeeTO;
 import java.io.FileNotFoundException;
 import java.sql.SQLException;
 import java.util.List;
@@ -10,18 +7,23 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
+import javax.persistence.NoResultException;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
-import org.junit.Assert;
 
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.qlrm.generator.ClassGenerator;
+import org.qlrm.model.Employee;
+import org.qlrm.to.EmployeeTO;
 
 public class JpaResultMapperTest {
 
     private static EntityManager em;
     private static JpaResultMapper jpaResultMapper = new JpaResultMapper();
     private static ClassGenerator classGenerator = new ClassGenerator();
+	private static int employeeId;
 
     @BeforeClass
     public static void init() throws ClassNotFoundException, SQLException, FileNotFoundException {
@@ -33,6 +35,7 @@ public class JpaResultMapperTest {
         e.setName("Peter Muster");
         em.persist(e);
         trx.commit();
+		employeeId = e.getId();
 
         // FIXME stefanheimberg: deaktiviert weil ohne generierte TO Objekte kompiliert dieser Test auch nicht.
         // generierung deaktiviert
@@ -63,6 +66,27 @@ public class JpaResultMapperTest {
         }
     }
 
+	@Test
+	public void listWithJpqlWhenUniqueResult() {
+		Query q = em.createQuery("SELECT e.id FROM Employee e");
+		List<Long> list = jpaResultMapper.list(q, Long.class);
+
+		Assert.assertNotNull(list);
+		Assert.assertEquals(1, list.size());
+		Assert.assertEquals(employeeId, list.get(0).longValue());
+	}
+
+	@Test
+	public void listWithJpqlWithNoResult() {
+		Query q = em.createQuery("SELECT e FROM Employee e WHERE e.id=?1");
+		q.setParameter(1, employeeId + 1);
+
+		List<Long> list = jpaResultMapper.list(q, Long.class);
+
+		Assert.assertNotNull(list);
+		Assert.assertTrue(list.isEmpty());
+	}
+
     @Test
     public void uniqueResultWithSql() {
         Query q = em.createNativeQuery("SELECT ID, NAME FROM EMPLOYEE WHERE ID = 1");
@@ -73,11 +97,29 @@ public class JpaResultMapperTest {
     }
 
     @Test
-    public void uniqueResultWithJpql() {
-        Query q = em.createNativeQuery("SELECT e.id, e.name FROM Employee e WHERE e.id = 1");
-        EmployeeTO to = jpaResultMapper.uniqueResult(q, EmployeeTO.class);
+	public void uniqueResultWithJpqlWhenSingleRow() {
+		Query q = em.createNativeQuery("SELECT e.id, e.name FROM Employee e WHERE e.id = 1");
+		EmployeeTO to = jpaResultMapper.uniqueResult(q, EmployeeTO.class);
 
-        Assert.assertNotNull(to);
-        System.out.println(to);
+		Assert.assertNotNull(to);
+		System.out.println(to);
+	}
+
+	@Test
+	public void uniqueResultWithJpqlWhenSingleResult() {
+		Query q = em.createQuery("SELECT COUNT(e) FROM Employee e");
+		Long result = jpaResultMapper.uniqueResult(q, Long.class);
+
+		Assert.assertNotNull(result);
+		Assert.assertEquals(employeeId, result.longValue());
     }
+
+	@Test(expected = NoResultException.class)
+	public void uniqueResultWithJpqlWhenNoResult() {
+		Query q = em.createQuery("SELECT e FROM Employee e WHERE e.id=?1");
+		q.setParameter(1, employeeId + 1);
+
+		jpaResultMapper.uniqueResult(q, Long.class);
+		Assert.fail("Expected " + NoResultException.class.getSimpleName() + " but not exception was thrown.");
+	}
 }
